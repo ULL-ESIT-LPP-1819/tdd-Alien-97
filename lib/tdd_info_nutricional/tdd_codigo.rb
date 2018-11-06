eb011cff5c034ed07e1f748c3ee27939da5026c6
alu0101035406@ull.edu.es <alu0101035406@ull.edu.es>
2018-11-06 09:40:18 +0000

    implementando codigo para corregir tercera prueba

--- lib/tdd_info_nutricional/tdd_codigo.rb
+++ lib/tdd_info_nutricional/tdd_codigo.rb
@@ -1,11 +1,12 @@
 #codigo valoracion nutricional
 
 class Tdd_valor_nutricional #attr_reader solo se declaran variables de instancia
-	attr_reader:g_saturadas,:azucares,proteinas
+	attr_reader:g_saturadas,:azucares,:proteinas
 
-	def initialize(g_saturadas, azucares)
+	def initialize(g_saturadas, azucares,proteinas)
 		@g_saturadas=g_saturadas
 		@azucares=azucares
+		@proteinas=proteinas
 	end
 end
 
